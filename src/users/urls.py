5f307d264ba22b5ae03da6464dcac07091e138c9
Maquiz1 <manquiz92@gmail.com>
2023-08-18 05:25:35 +0300

    first commit2

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path(r"accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
 
